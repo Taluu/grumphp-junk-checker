588a6c93372ca3b2f9b3047ac14e554c52b87826
Baptiste Clavié <clavie.b@gmail.com>
2020-05-12 13:56:10 +0200

    Use PSR-12

--- src/ExtensionLoader.php
+++ src/ExtensionLoader.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace GrumPHPJunkChecker;
 
 use GrumPHP\Extension\ExtensionInterface;
-
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
